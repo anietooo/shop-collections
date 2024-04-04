0f566cfb2e690b1378b43c98c7a47b2c4e64dfcd
alejandro <alejandro.nietotorres@educa.madrid.org>
2024-04-04 11:13:55 +0200

    finished(con compareTo)

--- src/main/java/org/ies/tierno/shop/model/Customers.java
+++ src/main/java/org/ies/tierno/shop/model/Customers.java
@@ -10,9 +10,21 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 
-public class Customers {
+public class Customers implements Comparable<Customers>{
     private String nif;
     private String name;
     private String surname;
     private List<Orders> orders;
+
+    @Override
+    public int compareTo(Customers o) {
+        int compare = this.surname.compareTo(o.surname);
+        if (compare == 0){
+            compare = this.name.compareTo(o.name);
+            if (compare == 0){
+                compare = this.nif.compareTo(o.nif);
+            }
+        }
+        return compare;
+    }
 }
